32135b1a991f411c3e6a302b90480b0e14a6bff3
Noob gamer <legofanslv@gmail.com>
2020-08-10 14:36:17 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace PlayingCards
+{
+    partial class Desk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Desk));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.loadCards = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadCards});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // loadCards
+            // 
+            this.loadCards.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.loadCards.Image = ((System.Drawing.Image)(resources.GetObject("loadCards.Image")));
+            this.loadCards.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.loadCards.Name = "loadCards";
+            this.loadCards.Size = new System.Drawing.Size(70, 22);
+            this.loadCards.Text = "Load Cards";
+            this.loadCards.Click += new System.EventHandler(this.loadCards_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton loadCards;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PlayingCards
+{
+    public partial class Desk : Form
+    {
+        private string folderPath = null;
+        private string[] fileNames = null;
+
+        public Desk()
+        {
+            InitializeComponent();
+            InitializeDesk();
+        }
+
+        private void InitializeDesk()
+        {
+            this.BackColor = Color.Green;
+        }
+
+        private void loadCards_Click(object sender, EventArgs e)
+        {
+            PictureBox filePictureBox = null;
+
+            folderPath = @"C:\A ROBERT FOTOGRAFE\Playing cards\Playing Cards\playing_card_images\face";
+            fileNames = Directory.GetFiles(folderPath);
+
+            //Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            foreach(var fileName in fileNames)
+            {
+                filePictureBox = new PictureBox()
+                {
+                    Height = 100,
+                    Width = 70,
+                    SizeMode = PictureBoxSizeMode.StretchImage,
+                    Left = 100,
+                    Top = 100,
+                    Image = Image.FromFile(fileName)
+                };
+                this.Controls.Add(filePictureBox);
+            }
+        }
+    }
+}
