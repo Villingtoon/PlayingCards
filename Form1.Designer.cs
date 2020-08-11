5f44683be67f4d7a8a1553824061862ef8c0e33b
Noob gamer <legofanslv@gmail.com>
2020-08-11 20:14:58 +0300

    ver 20:14

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,13 +31,17 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Desk));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.loadCards = new System.Windows.Forms.ToolStripButton();
+            this.stackCards = new System.Windows.Forms.ToolStripButton();
+            this.deckCards = new System.Windows.Forms.ToolStripButton();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStrip1
             // 
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.loadCards});
+            this.loadCards,
+            this.stackCards,
+            this.deckCards});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(800, 25);
@@ -54,13 +58,33 @@
             this.loadCards.Text = "Load Cards";
             this.loadCards.Click += new System.EventHandler(this.loadCards_Click);
             // 
-            // Form1
+            // stackCards
+            // 
+            this.stackCards.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.stackCards.Image = ((System.Drawing.Image)(resources.GetObject("stackCards.Image")));
+            this.stackCards.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.stackCards.Name = "stackCards";
+            this.stackCards.Size = new System.Drawing.Size(39, 22);
+            this.stackCards.Text = "Stack";
+            this.stackCards.Click += new System.EventHandler(this.stackCards_Click);
+            // 
+            // deckCards
+            // 
+            this.deckCards.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.deckCards.Image = ((System.Drawing.Image)(resources.GetObject("deckCards.Image")));
+            this.deckCards.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.deckCards.Name = "deckCards";
+            this.deckCards.Size = new System.Drawing.Size(37, 22);
+            this.deckCards.Text = "Deck";
+            this.deckCards.Click += new System.EventHandler(this.deckCards_Click);
+            // 
+            // Desk
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.toolStrip1);
-            this.Name = "Form1";
+            this.Name = "Desk";
             this.Text = "Form1";
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
@@ -73,6 +97,8 @@
 
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton loadCards;
+        private System.Windows.Forms.ToolStripButton stackCards;
+        private System.Windows.Forms.ToolStripButton deckCards;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,8 @@ namespace PlayingCards
     {
         private string folderPath = null;
         private string[] fileNames = null;
+        private Random rand = new Random();
+        private List<PictureBox> cards = new List<PictureBox>();
 
         public Desk()
         {
@@ -27,13 +29,29 @@ namespace PlayingCards
             this.BackColor = Color.Green;
         }
 
+        private string SelectFolder()
+        {
+            var selectFolderDialog = new FolderBrowserDialog();
+            DialogResult result = selectFolderDialog.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(selectFolderDialog.SelectedPath))
+            {
+                return selectFolderDialog.SelectedPath;
+            }
+            return null;
+        }
+
         private void loadCards_Click(object sender, EventArgs e)
         {
             PictureBox filePictureBox = null;
 
-            folderPath = @"C:\A ROBERT FOTOGRAFE\Playing cards\Playing Cards\playing_card_images\face";
-            fileNames = Directory.GetFiles(folderPath);
+            folderPath = SelectFolder();
+            if(folderPath == null)
+            {
+                return;
+            }
 
+            fileNames = Directory.GetFiles(folderPath);
             //Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
             foreach(var fileName in fileNames)
@@ -43,12 +61,45 @@ namespace PlayingCards
                     Height = 100,
                     Width = 70,
                     SizeMode = PictureBoxSizeMode.StretchImage,
-                    Left = 100,
-                    Top = 100,
+                    Left = rand.Next(0, 730),
+                    Top = rand.Next(0, 360),
                     Image = Image.FromFile(fileName)
                 };
+                filePictureBox.Click += Card_Click;
                 this.Controls.Add(filePictureBox);
+                cards.Add(filePictureBox);
             }
+        }
+
+        private void stackCards_Click(object sender, EventArgs e)
+        {
+            int x = 100, y = 100;
+            foreach(var card in cards)
+            {
+                card.Location = new Point(x, y);
+                x ++;
+                y ++;
+            }
+        }
+
+        private void deckCards_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            for (int x = 1; x < 10; x++)
+            {
+                for (int y = 1; y < 7; y++)
+                {
+                    cards[counter].Location = new Point(x * 80, y * 110);
+                    counter++;
+                }
+            }
+        }
+
+        private void Card_Click(object sender, EventArgs e)
+        {
+            var card = (PictureBox)sender;
+            card.Location = new Point(30, 30);
+            card.BringToFront();
         }
     }
 }
